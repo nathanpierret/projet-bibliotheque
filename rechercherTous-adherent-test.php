6b7a9c3df28cebeb553c6ec957aff87157bb30e6
Nathan Pierret <pierretnathan49@gmail.com>
2023-10-18 09:29:03 +0200

    Ajout du fichier pour rechercher tous les adhérents

---
+++ rechercherTous-adherent-test.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'bootstrap.php']);
+require "./vendor/autoload.php";
+
+use App\Entites\Adherent;
+
+$adherents = $entityManager->getRepository("App\\Entites\\Adherent")->findAll();
+
+foreach ($adherents as $adherent) {
+    echo $adherent->getId()."\n";
+    echo $adherent->getNumeroAdherent()."\n";
+    echo $adherent->getPrenom()."\n";
+    echo $adherent->getNom()."\n";
+    echo $adherent->getEmail()."\n";
+    echo $adherent->getDateAdhesion()->format("d/m/Y H:i:s")."\n";
+}
